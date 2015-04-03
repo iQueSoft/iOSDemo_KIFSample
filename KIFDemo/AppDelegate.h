6163f5493366978f98f336ac77a163afd08bdfcc
Oleg Vishnivetskiy <ovishnivetskiy@aericon.com>
2015-04-03 12:46:56 +0300

    Initial Commit

---
+++ KIFDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KIFDemo
+//
+//  Created by Oleg Vishnivetskiy on 03/04/15.
+//  Copyright (c) 2015 Oleg Vishnivetskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KIFDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KIFDemo
+//
+//  Created by Oleg Vishnivetskiy on 03/04/15.
+//  Copyright (c) 2015 Oleg Vishnivetskiy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
