508e4e49947e01d99fd230476c87c6a2d89e91d4
Oleg Vishnivetskiy <ovishnivetskiy@aericon.com>
2015-04-21 16:43:02 +0300

    parse app added

--- KIFDemo/AppDelegate.m
+++ KIFDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"7PEj64emp0cbzMBvjGwgXCEY7Gf3Oisb24Q2GsX4"
+                  clientKey:@"juL95JtJ1ZIG95mD8xcPNqViBqMYGtWZVSAiUyAz"];
     return YES;
 }
 
